7d3e98e6dcff92750aad9564196bf0c3f3a5fe37
chuy-zip <andreschuy03@gmail.com>
2025-05-18 19:14:49 -0600

    table type and calc of effectiveness

--- README.md
+++ README.md
@@ -3,8 +3,14 @@ Proyecto Final de Inteligencia Artificial
 
 
 Página visitada para selcción de movimientos:
+
 https://pokexperto.net/index2.php
 
 
 Página visitada para ver las stats de los pokemon
+
 https://pokemondb.net/pokedex
+
+Tabla de tipos basada en esta página (hemos ignorado el tipo hada):
+
+https://en.m.wikipedia.org/wiki/File:Pokemon_Type_Chart.svg
--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 from move_set_managament import load_pokemon_moves
 from pokemon_data import get_pokemon_stats
+from type_table import TypeTable
 
+# obteniendo la pool de los movimientos para cada pokemon inicial
 charizard_moves = load_pokemon_moves("charizard")
 blastoise_moves = load_pokemon_moves("blastoise")
 venusaur_moves = load_pokemon_moves("venusaur")
-
 print(charizard_moves,"\n")
 print(blastoise_moves,"\n")
 print(venusaur_moves,"\n")
 
-charizard_data = get_pokemon_stats("Charizard")
-print(charizard_data)
+# obteniendo las stats de cada pokemon (no tienen un set de 4 movimientos aun)
+charizard = get_pokemon_stats("Charizard")
+print(charizard)
+blastoise = get_pokemon_stats("Blastoise")
+print(blastoise)
+venusaur = get_pokemon_stats("Venusaur")
+print(venusaur)
 
-blastoise_data = get_pokemon_stats("Blastoise")
-print(blastoise_data)
+#traer la tabla de tipos y una prueba
+tabla_tipos = TypeTable()
 
-pikachu_data = get_pokemon_stats("Venusaur")
-print(pikachu_data)
+efectividad = tabla_tipos.calculate_effectivity("Roca", charizard["tipo"])
+print(f"La efectividad es de {efectividad}")
